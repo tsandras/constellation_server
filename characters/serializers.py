01029ccb050fb90ab1bce1cce0ecda4aedc622d8
Tristan Sandras <tristan@sandras.fr>
2024-01-28 18:11:14 +0100

    add skills and spells to node serializer

--- characters/serializers.py
+++ characters/serializers.py
@@ -1,8 +1,26 @@
 # from django.contrib.auth.models import Group, User
-from characters.models import Board, Node
+from characters.models import Board, Node, Skill
 from rest_framework import serializers
 
+class SkillSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Skill
+        fields = [
+            "id", "name", "description", "category"
+        ]
+
+class SpellSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Skill
+        fields = [
+            "id", "name", "description", "category"
+        ]
+
 class NodeSerializer(serializers.ModelSerializer):
+
+    skill = SkillSerializer(read_only=True)
+    spell = SpellSerializer(read_only=True)
+
     class Meta:
         model = Node
         fields = [
